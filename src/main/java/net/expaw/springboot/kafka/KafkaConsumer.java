bb05e96ae5337e730522f8c89eb1ae890da7df5f
exalt <exaltpawa@gmail.com>
2022-05-21 23:59:07 +0200

    Kafka Producer and Consumer implementation

---
+++ src/main/java/net/expaw/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package net.expaw.springboot.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "expaw", groupId = "myGroup")
+    public void consume(String message){
+        log.info(String.format("Message received %s",message));
+    }
+}
